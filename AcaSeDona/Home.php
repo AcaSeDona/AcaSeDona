c606fa3230026887763779c503565cbbe98a8415
ibooio <iboosocial@gmail.com>
2017-04-08 14:45:23 -0300

    google maps api key in env file
    Se añade en el archivo .env la api key de google maps ya que ahora es
    necesaria para el correcto funcionamiento

--- AcaSeDona/Home.php
+++ AcaSeDona/Home.php
@@ -19,6 +19,7 @@ class Home {
         Flight::view()->set("url_website", getWebsiteUrl(false) . '/');
         Flight::view()->set("recaptcha_public", getenv('RECAPTCHA_PUBLIC'));
         Flight::view()->set("ga_code", getenv('GOOGLE_ANALYTICS'));
+        Flight::view()->set("gm_api_key", getenv('GOOGLE_MAPS_API_KEY'));
 
         // layout assets
         Flight::view()->set("asset_favicon", getAssetUrl("assets/images/favicon.png"));
